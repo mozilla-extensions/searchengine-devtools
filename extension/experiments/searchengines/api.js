180e1bab6934c3848afb6b75498556eaf27f535b
Mark Banner <standard8@mozilla.com>
2022-07-04 11:17:45 +0100

    Bug 1777943 - Avoid unnecessarily importing XPCOMUtils and Services since the WebExtension Experiments API has already defined these.

--- extension/experiments/searchengines/api.js
+++ extension/experiments/searchengines/api.js
@@ -2,14 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* global ExtensionAPI */
-
-const { XPCOMUtils } = ChromeUtils.import(
-  "resource://gre/modules/XPCOMUtils.jsm"
-);
+/* global ExtensionAPI, XPCOMUtils, Services */
 
 XPCOMUtils.defineLazyModuleGetters(this, {
-  Services: "resource://gre/modules/Services.jsm",
   SearchEngineSelector: "resource://gre/modules/SearchEngineSelector.jsm",
 });
 
