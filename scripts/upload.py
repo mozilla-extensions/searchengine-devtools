c0f768fab91a0a334f1320f5072402dbf0a3dbb3
Mark Banner <standard8@mozilla.com>
2023-11-13 08:50:58 +0000

    chore: Handle search-config-v2 uploads.

--- scripts/upload.py
+++ scripts/upload.py
@@ -7,22 +7,29 @@ import requests
 import sys
 
 API_ENDPOINTS = {
-    'dev': "https://remote-settings-dev.allizom.org/v1/",
-    'stage': "https://remote-settings.allizom.org/v1/",
-    'prod': "https://remote-settings.mozilla.org/v1/"
+    "dev": "https://remote-settings-dev.allizom.org/v1/",
+    "stage": "https://remote-settings.allizom.org/v1/",
+    "prod": "https://remote-settings.mozilla.org/v1/",
 }
 
 parser = argparse.ArgumentParser(
     description="""
 Upload dump files to remote settings. It is assumed the dump files are in
 services/settings/dumps/main/ relative to the current directory.
-    """)
-parser.add_argument('-s', '--server',
-                    choices=['dev', 'stage', 'prod'],
-                    help='Which server to upload the collection to.')
-parser.add_argument('-c', '--collection',
-                    dest='collection',
-                    help='The collection to upload (e.g. search-telemetry)')
+    """
+)
+parser.add_argument(
+    "-s",
+    "--server",
+    choices=["dev", "stage", "prod"],
+    help="Which server to upload the collection to.",
+)
+parser.add_argument(
+    "-c",
+    "--collection",
+    dest="collection",
+    help="The collection to upload (e.g. search-telemetry)",
+)
 
 args = parser.parse_args()
 
@@ -30,17 +37,18 @@ if args.server is None or args.collection is None:
     parser.print_help()
     sys.exit(1)
 
-workspace = 'main' if args.server == 'dev' else 'main-workspace'
+# workspace = 'main' if args.server == 'dev' else 'main-workspace'
 
-API_ENDPOINT = API_ENDPOINTS[args.server] + \
-    "buckets/%s/collections/%s/records" % (workspace, args.collection)
+API_ENDPOINT = API_ENDPOINTS[args.server] + "buckets/%s/collections/%s/records" % (
+    "main-workspace",
+    args.collection,
+)
 
-authHeader = getpass('Enter Authentication Header (copy from site): ')
+authHeader = getpass("Enter Authentication Header (copy from site): ")
 headers = {"Authorization": authHeader}
 
 
-with open('services/settings/dumps/main/%s.json' % args.collection,
-          'r') as jsonFile:
+with open("services/settings/dumps/main/%s.json" % args.collection, "r") as jsonFile:
     data = jsonFile.read()
 
 records = json.loads(data)
@@ -60,7 +68,13 @@ def getIdForRecord(record):
     if args.collection == "search-config":
         return record["webExtension"]["id"]
 
-    return record["telemetryId"]
+    if "telemetryId" in record:
+        return record["telemetryId"]
+
+    if "identifier" in record:
+        return record["identifier"]
+
+    return record["recordType"]
 
 
 def findRecord(id, recordSet):
@@ -74,10 +88,10 @@ def findRecord(id, recordSet):
 
 def yes_or_no(question):
     while "the answer is invalid":
-        reply = str(inputFn(question+' (y/n): ')).lower().strip()
-        if reply[0] == 'y':
+        reply = str(inputFn(question + " (y/n): ")).lower().strip()
+        if reply[0] == "y":
             return True
-        if reply[0] == 'n':
+        if reply[0] == "n":
             return False
 
 
@@ -87,8 +101,7 @@ for record in records["data"]:
     existing = findRecord(getIdForRecord(record), existingRecords)
 
     if not existing:
-        response = requests.post(API_ENDPOINT, headers=headers,
-                                 json={"data": record})
+        response = requests.post(API_ENDPOINT, headers=headers, json={"data": record})
         if response.status_code != 200 and response.status_code != 201:
             print("BAD UPLOAD!")
             print(response)
@@ -107,10 +120,10 @@ for record in records["data"]:
         print("Up to date")
         continue
 
-    if yes_or_no('Upload changes to ' + getIdForRecord(record)):
-        response = requests.put(API_ENDPOINT + "/" + existingId,
-                                headers=headers,
-                                json={"data": record})
+    if yes_or_no("Upload changes to " + getIdForRecord(record)):
+        response = requests.put(
+            API_ENDPOINT + "/" + existingId, headers=headers, json={"data": record}
+        )
 
         print(response.status_code)
         if response.status_code != 200 and response.status_code != 201:
@@ -130,11 +143,12 @@ if len(recordsToRemove) > 0:
     for record in recordsToRemove:
         print(getIdForRecord(record))
 
-    if yes_or_no('Are you sure you wish to remove the above records?'):
+    if yes_or_no("Are you sure you wish to remove the above records?"):
         for record in recordsToRemove:
             print(getIdForRecord(record))
-            response = requests.delete(API_ENDPOINT + "/" + record["id"],
-                                       headers=headers)
+            response = requests.delete(
+                API_ENDPOINT + "/" + record["id"], headers=headers
+            )
             print(response.status_code)
             if response.status_code != 200 and response.status_code != 201:
                 print("BAD DELETE!")
