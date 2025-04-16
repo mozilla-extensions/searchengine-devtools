f7b56d72a8f3d3ce8a8d60aaa9ff3e1f6d458195
Mark Banner <standard8@mozilla.com>
2025-04-16 11:17:13 +0100

    fix: Update icon handling to match the latest requirements.
    This changes the icon handling to only filter by OS, since that's all we're using now. Additionally, move
    the getIcon function into Utils, for easier use later.
    It also updates the icon display to only have one column, since we're only aiming for one size per platform.

--- extension/content/utils.mjs
+++ extension/content/utils.mjs
@@ -2,7 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-let Utils = {
+class _Utils {
+  /**
+   * @type {string}
+   *   The current browser platform.
+   */
+  #platform = null;
+
   addDiv(element, content, data = "", className = "") {
     let div = document.createElement("div");
     div.textContent = content;
@@ -13,7 +19,7 @@ let Utils = {
       div.className = className;
     }
     element.appendChild(div);
-  },
+  }
 
   addImage(element, src, size, data = "") {
     let div = document.createElement("div");
@@ -27,7 +33,7 @@ let Utils = {
       div.appendChild(image);
     }
     element.appendChild(div);
-  },
+  }
 
   insertOptionList(field, list) {
     let fragment = document.createDocumentFragment();
@@ -37,19 +43,56 @@ let Utils = {
       fragment.appendChild(option);
     }
     field.appendChild(fragment);
-  },
+  }
 
   removeAllChildren(element) {
     while (element.firstChild) {
       element.firstChild.remove();
     }
-  },
+  }
 
   calculateTelemetryId(engineData) {
     return "telemetrySuffix" in engineData
       ? `${engineData.identifier}-${engineData.telemetrySuffix}`
       : engineData.identifier;
-  },
-};
+  }
 
+  /**
+   * Finds an icon in the remote settings records, according to the required
+   * size and returns the location.
+   *
+   * @param {string} engineIdentifier
+   *   The engine identifier.
+   * @param {number} iconSize
+   *   The requested size of the icon.
+   * @returns {string}
+   *   The location of the icon on the remote settings server.
+   */
+  async getIcon(iconConfig, engineIdentifier, iconSize) {
+    if (!this.#platform) {
+      this.#platform = (await browser.runtime.getPlatformInfo()).os;
+    }
+    for (let record of iconConfig?.data ?? []) {
+      if (
+        (!iconSize || record.imageSize == iconSize) &&
+        (!record.filter_expression ||
+          (await browser.experiments.searchengines.jexlFilterMatches(
+            record.filter_expression,
+            this.#platform
+          ))) &&
+        record.engineIdentifiers.some((i) => {
+          if (i.endsWith("*")) {
+            return engineIdentifier.startsWith(i.slice(0, -1));
+          }
+          return engineIdentifier == i;
+        })
+      ) {
+        return record.attachment.location;
+      }
+    }
+    return null;
+  }
+}
+
+const Utils = new _Utils();
 export default Utils;
