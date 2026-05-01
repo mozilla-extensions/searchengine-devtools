7c72c07340c6bebfc1394f0de9c7e50923e336f7
Mark Banner <standard8@mozilla.com>
2026-05-01 14:54:11 +0100

    Display images from all platforms, and allow linking to pdf based images for iOS

--- extension/content/utils.mjs
+++ extension/content/utils.mjs
@@ -3,12 +3,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 class _Utils {
-  /**
-   * @type {string}
-   *   The current browser platform.
-   */
-  #platform = null;
-
   addDiv(element, content, data = "", className = "") {
     let div = document.createElement("div");
     div.textContent = content;
@@ -45,25 +39,63 @@ class _Utils {
   }
 
   /**
-   * Adds or updates the image in the element.
+   * Adds multiple images in the element, indicating sizes if necessary.
    *
-   * @param {Element} parentElement
-   * @param {string} imageUrl
-   * @param {number} imageSize
+   * @param {Node} documentNode
+   * @param {string} baseUrl
+   * @param {{size: number, location: string, mimetype: string}[]} imageDetails
    * @param {string} data
    *   Any data to add as a property onto the div.
    */
-  addOrUpdateImage(parentElement, imageUrl, imageSize, data = "") {
-    if (
-      parentElement.firstChild?.tagName == "DIV" &&
-      parentElement.firstChild.firstChild?.tagName == "IMG" &&
-      parentElement.firstChild.firstChild?.getAttribute("size") == imageSize
-    ) {
-      parentElement.firstChild.firstChild.src = imageUrl;
-      return;
+  addImages(documentNode, baseUrl, imageDetails, data = "") {
+    let div = document.createElement("div");
+    if (data) {
+      div.data = data;
     }
-    this.removeAllChildren(parentElement);
-    this.addImage(parentElement, imageUrl, imageSize, data);
+
+    // For now, we only display one image, as we only ever ship one size of
+    // image in search-config-icons.
+    if (imageDetails.length) {
+      let anchor = document.createElement("a");
+      anchor.href = baseUrl + imageDetails[0].location;
+      anchor.target = "_blank";
+
+      if (imageDetails[0].mimetype == "application/pdf") {
+        anchor.textContent = "(pdf)";
+      } else {
+        let image = document.createElement("img");
+        image.src = baseUrl + imageDetails[0].location;
+        image.setAttribute("size", "16");
+        anchor.appendChild(image);
+      }
+      div.appendChild(anchor);
+    }
+    documentNode.appendChild(div);
+  }
+
+  /**
+   * Adds or updates the image in the element.
+   *
+   * @param {Node} documentNode
+   * @param {string} baseUrl
+   * @param {{size: number, location: string, mimetype: string}[]} imageDetails
+   * @param {string} data
+   *   Any data to add as a property onto the div.
+   */
+  addOrUpdateImages(documentNode, baseUrl, imageDetails, data = "") {
+    // TODO: Revisit this at some stage and consider making it work, so that
+    // when changing views, we're more efficient.
+    // if (
+    //   documentNode.firstChild?.tagName == "DIV" &&
+    //   documentNode.firstChild.firstChild?.tagName == "IMG"
+    //   documentNode.firstChild.firstChild?.getAttribute("size") == imageSize
+    // ) {
+    //   documentNode.firstChild.firstChild.src =
+    //     baseUrl + imageDetails[0].location;
+    //   return;
+    // }
+    this.removeAllChildren(documentNode);
+    this.addImages(documentNode, baseUrl, imageDetails, data);
   }
 
   insertOptionList(field, list) {
@@ -89,6 +121,17 @@ class _Utils {
   }
 
   /**
+   * @type {Map<string, boolean>}
+   *   A cache that is keyed by a record's filter_expression and the platform
+   *   being searched for. The value is the result of calling jexlFilterMatches
+   *   on the API.
+   *
+   *   This avoids repeatedly crossing the cross process boundary for each filter
+   *   call, hence speeding up display of icons, especially on the allEnginesView.
+   */
+  #filterCache = new Map();
+
+  /**
    * Filters search-config-icons, keeping records without a filter expression or
    * with expressions that matches the current platform. Only supports filtering
    * by OS, as that is all we currently require for search-config-icons.
@@ -96,19 +139,31 @@ class _Utils {
    * @param {object[]} unfilteredConfig
    * @returns object[]
    */
-  async filterIconConfig(unfilteredConfig) {
-    if (!this.#platform) {
-      this.#platform = (await browser.runtime.getPlatformInfo()).os;
-    }
+  async filterIconConfig(unfilteredConfig, platform) {
     let result = [];
     for (let record of unfilteredConfig.data) {
+      if (!record.filter_expression) {
+        result.push(record);
+        continue;
+      }
+
       if (
-        !record.filter_expression ||
-        (await browser.experiments.searchengines.jexlFilterMatches(
-          record.filter_expression,
-          this.#platform
-        ))
+        this.#filterCache.has(record.filter_expression + platform) != undefined
       ) {
+        if (this.#filterCache.get(record.filter_expression + platform)) {
+          result.push(record);
+          continue;
+        }
+      }
+
+      let matches = await browser.experiments.searchengines.jexlFilterMatches(
+        record.filter_expression,
+        platform
+      );
+
+      this.#filterCache.set(record.filter_expression + platform, matches);
+
+      if (matches) {
         result.push(record);
       }
     }
@@ -116,22 +171,23 @@ class _Utils {
   }
 
   /**
-   * Finds an icon in the remote settings records, according to the required
-   * size and returns the location.
+   * Finds any matching icon records in the remote settings records.
    *
    * @param {object[]} iconConfig
-   *   The filtered array of icon records from search-config-icons.
+   *   The array of icon records from search-config-icons.
    * @param {string} engineIdentifier
    *   The engine identifier.
-   * @param {number} iconSize
-   *   The requested size of the icon.
-   * @returns {string}
-   *   The location of the icon on the remote settings server.
+   * @param {string} platform
+   *   The platform to get the icons for.
    */
-  getIcon(iconConfig, engineIdentifier, iconSize) {
-    for (let record of iconConfig ?? []) {
+  async getIcons(iconConfig, engineIdentifier, platform) {
+    let filteredConfig = await this.filterIconConfig(iconConfig, platform);
+
+    /** @type {{size: number, location: string, mimetype: string}[]} */
+    let icons = [];
+
+    for (let record of filteredConfig) {
       if (
-        (!iconSize || record.imageSize == iconSize) &&
         record.engineIdentifiers.some((i) => {
           if (i.endsWith("*")) {
             return engineIdentifier.startsWith(i.slice(0, -1));
@@ -139,10 +195,14 @@ class _Utils {
           return engineIdentifier == i;
         })
       ) {
-        return record.attachment.location;
+        icons.push({
+          size: record.imageSize,
+          location: record.attachment.location,
+          mimetype: record.attachment.mimetype,
+        });
       }
     }
-    return null;
+    return icons;
   }
 }
 
